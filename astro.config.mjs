d88a0b177678fd2dd307c6e5d6b9cecc9747b036
Joey Nichols <joeyquarters@gmail.com>
2024-01-28 14:30:06 -0500

    Complete the Astro blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jazzy-naiad-762a14.netlify.app/",
+  integrations: [preact()]
+});
